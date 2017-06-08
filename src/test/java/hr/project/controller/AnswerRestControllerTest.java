a4c6265dcaf12fff5be5fe0ebdc5f041007678bc
Marko Šojat <markosojat1@gmail.com>
2017-06-08 21:20:48 +0200

    Created findAllTest method.

--- src/test/java/hr/project/controller/AnswerRestControllerTest.java
+++ src/test/java/hr/project/controller/AnswerRestControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -22,8 +24,18 @@ public class AnswerRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private AnswerRestController answerRestController;
+
     @Test
-    public void shouldReturnObject() throws Exception {
+    public void findAllTest(){
+        assertThat(answerRestController.findAll()).isNotNull();
+    }
+
+    @Test
+    public void getTest() throws Exception {
         this.mockMvc.perform(get("/answer")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(notNullValue()));
     }
+
+
 }
