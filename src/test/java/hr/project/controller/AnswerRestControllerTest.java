8d0f9ff4f4f7395759252334f182b7540fbb2d50
Marko Šojat <markosojat1@gmail.com>
2017-06-09 07:43:30 +0200

    Created postTest.

--- src/test/java/hr/project/controller/AnswerRestControllerTest.java
+++ src/test/java/hr/project/controller/AnswerRestControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +32,20 @@ public class AnswerRestControllerTest {
     @Test
     public void findAllTest(){
         assertThat(answerRestController.findAll()).isNotNull();
+    }
+
+    @Test
+    public void postTest() throws Exception {
+        this.mockMvc.perform(post("/answer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"id\": 3,\n" +
+                        "    \"answer\": \"255\",\n" +
+                        "    \"correct\": false,\n" +
+                        "    \"question_id\": 1\n" +
+                        "}")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
     }
 
     @Test
